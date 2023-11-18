e5112ed9c155a9259df3ace617ea32ed1ecf3f6d
AjayBiradar5956 <ajay99biradar@gmail.com>
2023-11-18 11:07:34 +0530

    handled edit feature and updated the code

--- src/app/todo-input/item-detail/item-detail.component.ts
+++ src/app/todo-input/item-detail/item-detail.component.ts
@@ -35,13 +35,30 @@ export class ItemDetailComponent {
     // Update editedTask when task changes
     if ('task' in changes) {
       this.editedTask.id = this.task.id;
+      this.editedTask.name = this.task.name;
+      this.editedTask.date = this.task.date;
+      this.editedTask.priority = this.task.priority;
     }
   }
 
+  changedName(e: any) {
+    this.editedTask.name = e.target.value;
+  }
+
+  changedDate(e: any) {
+    this.editedTask.date = e.target.value;
+  }
+
+  changedPriority(e: any) {
+    this.editedTask.priority = e.target.value;
+  }
+
   @Output()
-  onEditTaskButtonClicked: EventEmitter<Object> = new EventEmitter<Object>;
+  onEditTaskButtonClicked: EventEmitter<Object> = new EventEmitter<Object>();
+
   editTask() {
     this.onEditTaskButtonClicked.emit(this.editedTask);
+
     var doc = document.querySelector('.offcanvas');
     var doc1 = document.querySelector('.fade');
     var body = document.body;
@@ -50,13 +67,11 @@ export class ItemDetailComponent {
     if (doc1) {
       doc1.remove();
     }
-    //reset values
+
+    // reset values
     this.editedTask.id = 0;
     this.editedTask.name = '';
     this.editedTask.priority = '';
     this.editedTask.date = null;
-
   }
 }
-
-
